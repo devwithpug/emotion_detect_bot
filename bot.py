ec5c573b621608fb5cc51f4b6c2b0998604d75e0
Jungyu Choi <zmfjscl789@gmail.com>
2021-03-06 22:17:32 +0900

    210306

--- bot.py
+++ bot.py
@@ -9,6 +9,11 @@ from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from PIL import Image, ImageDraw, ImageFont
+import sys, io
+#sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+
+font = ImageFont.truetype('Bazzi.ttf', 30)
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -42,24 +47,24 @@ model.add(Dense(7, activation="softmax"))
 mode = "display"
 
 model.load_weights("model.h5")
-print("model loaded")
+
 
 # prevents openCL usage and unnecessary logging messages
 cv2.ocl.setUseOpenCL(False)
 
 # dictionary which assigns each label an emotion (alphabetical order)
 emotion_dict = {
-    0: "Angry",
-    1: "Disgusted",
-    2: "Fearful",
-    3: "Happy",
-    4: "Neutral",
-    5: "Sad",
-    6: "Surprised",
+    0: "화난",
+    1: "구역질나는",
+    2: "무서운",
+    3: "행복한",
+    4: "무표정",
+    5: "슬픈",
+    6: "놀란",
 }
 
 facecasc = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-print("haarcascade loaded")
+
 
 
 def emotion_detect(url):
@@ -67,10 +72,10 @@ def emotion_detect(url):
     raw = requests.get(url).content
     nparr = np.asarray(bytearray(raw), dtype=np.uint8)
     frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    print("1")
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-    print("2")
+    
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
         roi_gray = gray[y : y + h, x : x + w]
@@ -80,41 +85,62 @@ def emotion_detect(url):
         prediction = model.predict(cropped_img)
         print(prediction)
         maxindex = int(np.argmax(prediction))
-        cv2.putText(
-            frame,
-            emotion_dict[maxindex],
-            (x + 20, y - 5),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (255, 255, 255),
-            2,
-            cv2.LINE_AA,
-        )
-    print("3")
+        
+        print("DEBUG: {}".format(maxindex))
+        text_img = Image.fromarray(frame)
+        draw = ImageDraw.Draw(text_img)
+        
+        draw.text((x+5, y-30), emotion_dict[maxindex], font=font, fill=(255, 255, 255))
+        
+        frame = np.array(text_img)
+        # cv2.putText(
+        #     frame,
+        #     emotion_dict[maxindex],
+        #     (x + 20, y - 5),
+        #     cv2.FONT_HERSHEY_SIMPLEX,
+        #     1,
+        #     (255, 255, 255),
+        #     2,
+        #     cv2.LINE_AA,
+        # )
+    try:
+        print("result: ", emotion_dict[maxindex])
+    except Exception as err:
+        print(err)
+        return
+
     return frame
 
 
 @bot.event
 async def on_ready():
-    print("봇이 시작되었습니다.")
+    print("BOT IS READY NOW!")
 
 
 @bot.command()
 async def 표정(ctx, *args):
+    if not args:
+        await ctx.send("`~표정 (이미지 주소)`")
+        return
     try:
         raw = requests.get(args[0]).content
     except Exception as err:
-        await ctx.send("이미지 디코딩 실패: " + err)
+        await ctx.send("이미지 디코딩 실패: {}".format(err))
         return
     after_image = emotion_detect(args[0])
+    
     try:
         is_success, buffer = cv2.imencode(".png", after_image)
     except Exception as err:
-        await ctx.send("이미지 인코딩 실패: " + err)
+        await ctx.send("`얼굴을 인식할 수 없습니다.`")
         return
+    
     io_buf = BytesIO(buffer.tobytes())
-
-    await ctx.send(file=discord.File(fp=io_buf, filename="image.png"))
+    try:
+        await ctx.send(file=discord.File(fp=io_buf, filename="image.png"))
+    except Exception:
+        await ctx.send('`이미지 크기가 너무 큽니다.`')
+        return
 
 
 bot.run(token)
